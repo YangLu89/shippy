92a72b938501f6e3a715e1b2d679d6d5ae7f254a
yanglu89 <yanglu6120@gmail.com>
2019-09-25 16:48:09 -0700

    t

--- consignment-service/main.go
+++ consignment-service/main.go
@@ -5,6 +5,7 @@ import (
 	// Import the generated protobuf code
 	"context"
 	"fmt"
+	"log"
 	"sync"
 
 	pb "github.com/YangLu89/shippy/consignment-service/proto/consignment"
@@ -51,7 +52,28 @@ type service struct {
 // which is a create method, which takes a context and a request as an
 // argument, these are handled by the gRPC server.
 func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, res *pb.Response) error {
-	vesselResponse, err := s.vesselClient
+
+	// Here we call a client instance of our vessel service with our consignment weight,
+	// and the amount of containers as the capacity value
+	vesselResponse, err := s.vesselClient.FindAvailable(context.Background(), &vesselProto.Specification{
+		MaxWeight: req.Weight,
+		Capacity:  int32(len(req.Containers)),
+	})
+
+	log.Printf("Found vessel: %s \n", vesselResponse.Vessel.Name)
+	if err != nil {
+		return err
+	}
+
+	// Save our consignment
+	consignment, err := s.repo.Create(req)
+	if err != nil {
+		return err
+	}
+
+	// We set the VesselId as the vessel we got back from our
+	// vessel service
+	req.VesselId = vesselResponse.Vessel.Id
 
 	// Save our consignment
 	consignment, err := s.repo.Create(req)
