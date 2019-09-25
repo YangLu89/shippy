2cc411a33a4d842077377067366cc30fc653bfa7
yanglu89 <yanglu6120@gmail.com>
2019-09-24 17:56:35 -0700

    update docker file for consignment service

--- consignment-cli/cli.go
+++ consignment-cli/cli.go
@@ -10,7 +10,7 @@ import (
 	"context"
 
 	pb "github.com/YangLu89/shippy/consignment-service/proto/consignment"
-	"google.golang.org/grpc"
+	micro "github.com/micro/go-micro"
 )
 
 const (
@@ -30,12 +30,9 @@ func parseFile(file string) (*pb.Consignment, error) {
 
 func main() {
 	// Set up a connection to the server.
-	conn, err := grpc.Dial(address, grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("Did not connect: %v", err)
-	}
-	defer conn.Close()
-	client := pb.NewShippingServiceClient(conn)
+	service := micro.NewService(micro.Name("shippy.consignment_cli"))
+	service.Init()
+	client := pb.NewShippingServiceClient("consignment", service.Client())
 
 	// Contact the server and print out its response.
 	file := defaultFilename
--- consignment-service/go.mod
+++ consignment-service/go.mod
@@ -4,5 +4,6 @@ go 1.13
 
 require (
 	github.com/golang/protobuf v1.3.2
-	google.golang.org/grpc v1.23.1
+	github.com/micro/go-micro v1.10.0
+	golang.org/x/net v0.0.0-20190923162816-aa69164e4478
 )
